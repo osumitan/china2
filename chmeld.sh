062ba1f9bf665460933f57a1905ad24a941801e4
osumitan <osumitan@gmail.com>
2017-03-04 08:57:09 +0900

    chmeld.sh を追加

---
+++ chmeld.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+LOG_HOME=/home/sysadmin/max3tb/data/develop/sh/china2/log
+PARAM=$1
+PREFIX=${LOG_HOME}/${PARAM}
+
+CNT=`ls ${PREFIX}* | wc -l`
+if [ ${CNT} -ne 2 ]
+then
+	echo "illegal file count: ${PARAM} ${CNT}"
+	exit 1
+fi
+
+FILEA=`ls ${PREFIX}* | head -1`
+FILEB=`ls ${PREFIX}* | head -2 | tail -1`
+
+meld ${FILEA} ${FILEB}
+
